e6e3b510fa68325e98779c6cd839254c10990cf7
Xen00n <nikunj11pokharel@gmail.com>
2025-12-08 17:28:13 +0545

    refactoring serializer to be done with all dtos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,33 +2,31 @@ import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Q
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService) { }
     @Post('signup')
     async createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password);
     }
-    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
-        if(!user){
+        if (!user) {
             throw new NotFoundException('User not found');
         }
         return user;
     }
-
     @Get()
     async findAllUsers(@Query("email") email: string) {
         return await this.usersService.find(email);
     }
-    @UseInterceptors(SerializeInterceptor)
     @Patch('/:id')
-    async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) 
-    {
+    async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
         return await this.usersService.update(parseInt(id), body);
     }
     @Delete('/:id')
