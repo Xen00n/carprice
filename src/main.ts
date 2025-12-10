de8d15f799427384407365952c2b3110fa56565b
Xen00n <nikunj11pokharel@gmail.com>
2025-12-10 11:30:25 +0545

    Added cookie session and made cookie to store userId

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+const cookieSesion = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSesion({
+    keys: ['asdfdf2341234adsf']
+  }));
   app.useGlobalPipes(new ValidationPipe(
-{
+    {
       whitelist: true,
     },
   ));
